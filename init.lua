186ec952051076584de4eefc30de26356e1f31af
gremble0 <hermanstornes@gmail.com>
2023-12-06 00:10:07 +0100

    Basic functioning telescope picker

--- init.lua
+++ init.lua
@@ -17,6 +17,30 @@ function M.add_to_kill_ring(value)
   end
 end
 
+function M.paste_from_kill_ring(opts) -- TODO: move parts of function to UI module
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  local conf = require("telescope.config").values
+
+  pickers.new(opts, {
+    prompt_title = "Paste from kill ring",
+    finder = finders.new_table {
+      results = M.values,
+    },
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, _)
+      actions.select_default:replace(function ()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        vim.api.nvim_put({ selection[1] }, "l", true, true)
+      end)
+      return true
+    end
+  }):find()
+end
+
 --- @param opts? KillRingConfig
 function M.setup(opts)
   M.config = config.get_config(opts)
