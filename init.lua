1398329f939cfe7403b0bbae80157faefdeb87d6
gremble0 <hermanstornes@gmail.com>
2023-12-05 21:05:19 +0100

    add_to_kill_ring function working

--- init.lua
+++ init.lua
@@ -3,22 +3,31 @@ local config = require("killring.config")
 --- @class KillRing
 --- @field config KillRingConfig
 --- @field cur_size integer
+--- @field values string[] TODO: make own class for this?
 local M = {}
 
 --- @param value string
 function M:add_to_kill_ring(value)
-  self.cur_size = self.cur_size + 1
-  self[self.cur_size] = value
-  print("YO")
+  -- rotate killring
+  for i = self.cur_size, 2, -1 do
+    self.values[i] = self.values[i - 1]
+  end
+  self.values[1] = value
+
+  if self.cur_size < self.config.max_size then
+    self.cur_size = self.cur_size + 1
+  end
+  print(vim.inspect(self.values))
 end
 
 --- @param opts? table<string, any>
-function M.setup(opts)
+function M:setup(opts)
   M.config = config.get_config(opts)
   M.cur_size = 0
+  M.values = {}
 
   vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = M:add_to_kill_ring(vim.fn.getreg('"'))
+    callback = function() M:add_to_kill_ring(vim.fn.getreg('"')) end
   })
 end
 
