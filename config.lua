df86ade0b912e3cbf3256f781174db3a80b908ac
gremble0 <hermanstornes@gmail.com>
2023-12-06 01:07:01 +0100

    Fixed newlines parsing for ui picker

--- config.lua
+++ config.lua
@@ -1,15 +1,16 @@
---- @alias OpenKind "telescope" | "split"
+---@alias OpenKind "telescope" | "split"
 -- TODO: implement UI ^
 
---- @class KillRingConfig
---- @field max_size? integer
---- @field open_kind? OpenKind
---- @field buffer_local? boolean
+---@class KillRingConfig
+---@field max_size? integer
+---@field open_kind? OpenKind
+---@field buffer_local? boolean
+---@field line_separator? string
 local M = {}
 
 --- Merges users configuration specified in opts with the default config
---- @param opts? KillRingConfig
---- @return KillRingConfig
+---@param opts? KillRingConfig
+---@return KillRingConfig
 function M.get_config(opts)
   local config = M.get_default_config()
 
@@ -22,12 +23,13 @@ function M.get_config(opts)
   return config
 end
 
---- @return KillRingConfig
+---@return KillRingConfig
 function M.get_default_config()
   return {
     max_size = 10,
     open_kind = "telescope", -- < TODO: implement open UI
     buffer_local = false,
+    line_separator = "󰌑"
   }
 end
 
--- init.lua
+++ init.lua
@@ -1,19 +1,45 @@
 local config = require("killring.config")
 
---- @class KillRing
---- @field config KillRingConfig
---- @field values string[] TODO: make own class for this?
+---@class KillRing
+---@field config KillRingConfig
+---@field values string[][] TODO: make own class for this? (list of values in kill ring, nested array for multiline values)
 local M = {}
 
---- @param value string
+---@param s string
+---@return string[]
+function M._split_by_newlines(s) -- TODO: move to separate module
+  local lines = {}
+
+  for line, _ in s:gmatch("[^\n\r]+") do
+    table.insert(lines, line)
+  end
+
+  return lines
+end
+
+---@param lines string[]
+---@return string
+function M._cat_newlines(lines)
+  local s = ""
+
+  for _, line in ipairs(lines) do
+    s = s .. line .. M.config.line_separator
+  end
+
+  return s
+end
+
+---@param value string
 function M.add_to_kill_ring(value)
+  local parsed_value = M._split_by_newlines(value)
+
   if #M.values < M.config.max_size then
-    M.values[#M.values + 1] = value
+    M.values[#M.values + 1] = parsed_value
   else
     for i = #M.values, 2, -1 do
       M.values[i] = M.values[i - 1]
     end
-    M.values[1] = value
+    M.values[1] = parsed_value
   end
 end
 
@@ -24,24 +50,29 @@ function M.paste_from_kill_ring(opts) -- TODO: move parts of function to UI modu
   local action_state = require("telescope.actions.state")
   local conf = require("telescope.config").values
 
+  local catted_values = {}
+  for _, value in ipairs(M.values) do
+    table.insert(catted_values, M._cat_newlines(value))
+  end
+
   pickers.new(opts, {
     prompt_title = "Paste from kill ring",
     finder = finders.new_table {
-      results = M.values,
+      results = catted_values, -- TODO: self and ':' things?
     },
     sorter = conf.generic_sorter(opts),
     attach_mappings = function(prompt_bufnr, _)
       actions.select_default:replace(function ()
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        vim.api.nvim_put({ selection[1] }, "l", true, true)
+        vim.api.nvim_put(M.values[selection.index], "l", true, true)
       end)
       return true
     end
   }):find()
 end
 
---- @param opts? KillRingConfig
+---@param opts? KillRingConfig
 function M.setup(opts)
   M.config = config.get_config(opts)
   M.values = {}
